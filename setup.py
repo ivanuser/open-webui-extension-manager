a0b75018393a82f426a10b1c391f4e4081768b14
ivanuser <honerivan@gmail.com>
2025-03-12 15:11:48 -0400

    update

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="open-webui-extensions",
-    version="0.1.0",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        "fastapi>=0.95.0",
-        "pydantic>=2.0.0",
-        "jinja2>=3.0.0",
-        "aiofiles>=0.8.0",
-        "click>=8.0.0",
-    ],
-    entry_points={
-        "console_scripts": [
-            "openwebui-ext=open_webui_extensions.cli:main",
-        ],
-    },
-    python_requires=">=3.8",
-)
+setup()
